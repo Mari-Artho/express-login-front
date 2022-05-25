8e8f27830470817af8051a14a1e9a47a39b6cbf1
Mari-Artho <mari.artho@medieinstitutet.se>
2022-05-25 16:11:01 +0200

    Create front files and connected to backenCreate front files and connected to backend

---
+++ script.js
@@ -0,0 +1,50 @@
+let userList = document.getElementById('loginForm');
+
+    userList.innerHTML ="";
+
+// fetch("http://localhost:4000/api/post/:key")
+fetch("http://localhost:4000/api/post")
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+
+    data.forEach(user => {
+        let userItem = document.createElement('li');
+        userItem.innerHTML = `${user.email} ${user.password}`;
+        userList.appendChild(userItem);
+    })
+});
+
+document.getElementById('loginBtn').addEventListener('click', (e)=>{
+
+    e.preventDefault();
+
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    let user = {
+        email: email,
+        password: password
+    };
+
+    fetch('http://localhost:4000/api/postß', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+
+        if (data === 'success'){
+            console.log('It works!This is from script.js(front)');
+            //Save to Localstorage
+            let userId = data.userId;
+        } else {
+            console.log('It does not works!This is from script.js(front)');
+        }
+    });
+
+});

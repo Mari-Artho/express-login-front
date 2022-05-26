21946c0ac9b91ce2ce0c9c124fa409afc67b49ca
Mari-Artho <mari.artho@medieinstitutet.se>
2022-05-26 13:20:59 +0200

    Create log in button, but can't log in.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let userList = document.getElementById('loginForm');
+//Sign up form 
+let userList = document.getElementById('signupForm');
 userList.innerHTML ="";
 
 fetch("http://localhost:4000/api/post")
@@ -8,17 +9,18 @@ fetch("http://localhost:4000/api/post")
 
     data.forEach(user => {
         let userItem = document.createElement('li');
-        userItem.innerHTML = `email/user name =>  ${user.email} <br> password =>  ${user.password}`;
+        userItem.innerHTML = ` ${user.email} <br>  ${user.password}`;
         userList.appendChild(userItem);
     })
 });
 
-document.getElementById('loginBtn').addEventListener('click', (e)=>{
+//Sign up button
+document.getElementById('signupBtn').addEventListener('click', (e)=>{
 
     e.preventDefault();
 
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
+    let email = document.getElementById('signupEmail').value;
+    let password = document.getElementById('signupPassword').value;
 
     let user = {
         email: email,
@@ -39,9 +41,44 @@ document.getElementById('loginBtn').addEventListener('click', (e)=>{
         if (data === 'success'){
             console.log('It works!This is from script.js(front)');
             //Save to Localstorage
-            // let userId = data.userId;
+             let userId = data.userId;
         } else {
             console.log('It does not works!This is from script.js(front)');
+        }
+    });
+
+});
+
+//Log in button
+document.getElementById('loginBtn').addEventListener('click', (e)=>{
+
+    e.preventDefault();
+
+    let loginEmail = document.getElementById('email').value;
+    let loginPassword = document.getElementById('password').value;
+
+    let user = {
+        email: loginEmail,
+        password: loginPassword
+    };
+
+    fetch('http://localhost:4000/api/post', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+       body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+
+        if (data === 'success'){
+            console.log('Log in　成功だよ！！');
+            //Save to Localstorage
+            //let userId = data.userId;
+        } else {
+            console.log('ログイン失敗だよ！！泣く！！');
         }
     });
 

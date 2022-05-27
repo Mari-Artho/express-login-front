fa72de6c72ac4da09b67ca36913f7d0897394ee4
Mari-Artho <mari.artho@medieinstitutet.se>
2022-05-27 17:59:54 +0200

    Create Admin page

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-//show html after login success
-const loginResult = document.getElementById("loginResult");
-
-//Show all sign up users at list
-let userList = document.getElementById('signupForm');
-userList.innerHTML ="";
-
-fetch("http://localhost:4000/users")
-.then(res => res.json())
-.then(data => {
-    console.log(data);
-
-    data.forEach(user => {
-        let userItem = document.createElement('li');
-        userItem.innerHTML = ` ${user.email}, ${user.password}, subscribed: ${user.subscribe}<br>`;
-        userList.appendChild(userItem);
-    })
-});
-
 //Sign up button
 document.getElementById('signupBtn').addEventListener('click', (e)=>{
 
@@ -64,10 +45,40 @@ document.getElementById('loginBtn').addEventListener('click', (e)=>{
     })
     .then(res => res.json()) // parse result
     .then(data => {
-        if (data.email != ""){ // empty user data <=> login failed
+        if((email == "admin" && password == "admin")){
+    //hide section(login) area.
+    document.querySelector("section").style.display = "none";
+    document.getElementById("loginResult").style.display = "none";
+    //admin message
+    let admin = document.getElementById("adminMessage");
+    let messageAdmin = document.createElement('div');
+     messageAdmin.innerHTML = `Admin page`;
+     admin.appendChild(messageAdmin);
+      
+    //show html after login success
+    const loginResult = document.getElementById("loginResult");
+    //Show all sign up users at list
+    let userList = document.getElementById('signupForm');
+    userList.innerHTML ="";
+
+    fetch("http://localhost:4000/users")
+    .then(res => res.json())
+    .then(data => {
+    console.log(data);
+
+    data.forEach(user => {
+        let userItem = document.createElement('li');
+        userItem.innerHTML = ` ${user.email}, ${user.password}, subscribed: ${user.subscribe}<br>`;
+        userList.appendChild(userItem);
+       
+    })
+});
+
+        }
+        else if (data.email != ""){ 
+            // empty user data <=> login failed
             console.log('Log in 成功だよ！！'); // user info is in "data"
             setLoggedInScreen(data);
-            
         } else {
             loginFailMessage();
         }
